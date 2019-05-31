3a2e07dec42b9bbe3e23f039523bd701c3ef89ff
Garrett Weems <gwgraphicdesign@gmail.com>
2019-05-31 18:36:45 -0400

    ♻️ Moved theme related objects to a new file
    Refactored Layout component to import theme related objects from a new file 'theme.js' located in 'src/theme.js'

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,12 @@
-import { StaticQuery, graphql } from 'gatsby'
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import PropTypes from 'prop-types'
 import React from 'react'
-import Container from '@material-ui/core/Container'
+import { StaticQuery, graphql } from 'gatsby'
+import PropTypes from 'prop-types'
+import { ThemeProvider } from 'styled-components'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import { theme, muiTheme, GlobalStyle } from '../theme'
 import Navbar from './navbar'
-
-// Light: #f2f5f8
-// Dark: #303030
-// Grey #454545
-// Red #e74c3d
-// Blue #035985
-// Yellow #ffba29
-
-const theme = {
-  colors: {
-    light: '#f2f5f8',
-    dark: '#303030',
-    grey: '#454545',
-    red: '#e74c3d',
-    blue: '#035985',
-    yellow: '#ffba29',
-  },
-}
-
-const muiTheme = createMuiTheme({
-  palette: {
-    primary: {
-      light: theme.colors.light,
-      main: theme.colors.red,
-      secondary: theme.colors.blue,
-      dark: theme.colors.dark,
-      contrastText: theme.colors.light,
-    },
-    secondary: {
-      light: theme.colors.light,
-      main: theme.colors.dark,
-      dark: theme.colors.yellow,
-      contrastText: theme.colors.light,
-    },
-    // error: will use the default color
-  },
-})
-
-const GlobalStyle = createGlobalStyle`
-a { all: unset; }
-a { :hover { cursor: pointer; } }
-`
 
 const Layout = ({ children }) => (
   <StaticQuery
