8756687834f34ae1823d6d8655a801f20df2f4f2
Tristan Grovender <therealtristangrovender@gmail.com>
2019-06-13 14:58:39 -0500

    linting changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,8 +2,8 @@ import AppBar from '@material-ui/core/AppBar'
 import Button from '@material-ui/core/Button'
 import React from 'react'
 import Toolbar from '@material-ui/core/Toolbar'
-import Typography from "@material-ui/core/Typography"
-import { makeStyles } from "@material-ui/core/styles"
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 
