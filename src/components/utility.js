a567a40890534a2a92a4fa94dd5f36b83cdbb38a
Alan Perez <alanp9six@gmail.com>
2019-06-28 10:39:05 -0500

    fixed  merge conflict

--- src/components/utility.js
+++ src/components/utility.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { palette, spacing, borders } from '@material-ui/system'
 
 export const Flex = styled.div`
   width: 100%;
@@ -7,7 +8,6 @@ export const Flex = styled.div`
   justify-content: ${props => props.justify};
   align-items: ${props => props.align};
 `
-
 
 export const InputWrapper = styled.div`
   display: flex;
@@ -18,4 +18,19 @@ export const InputWrapper = styled.div`
     margin-left: 1rem;
   }
 `
+export const Box = styled.div`
+  ${palette}
+  ${spacing}
+  ${borders}
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: fit-content;
+  button {
+    margin-left: 1rem;
+  }
+`
 
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
