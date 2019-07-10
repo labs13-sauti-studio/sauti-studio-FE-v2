2da04268a7e0991031f8daeecc5d2031ec5982b5
amberm <nekosenpai@gmail.com>
2019-07-10 17:19:08 -0600

    basic components made

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import Home from './pages/home.js';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
-      <body>
-        <p> Design your own Flow</p>
-      </body>
+      <Home /> 
+      
     </div>
   );
 }
