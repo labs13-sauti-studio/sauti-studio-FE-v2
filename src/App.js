556d4b75544b2b4fa853f33f4533d4cb26102dfb
amberm <nekosenpai@gmail.com>
2019-07-10 16:55:20 -0600

    file structure setup

--- src/App.js
+++ src/App.js
@@ -6,19 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <body>
+        <p> Design your own Flow</p>
+      </body>
     </div>
   );
 }
