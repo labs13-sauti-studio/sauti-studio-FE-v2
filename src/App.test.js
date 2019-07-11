cb90dfde0a44f099e42fd4886594b695b94752e2
amberm <nekosenpai@gmail.com>
2019-07-11 17:06:59 -0600

    can test standard react components, but no BRouter ones

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+// test('Fake Test', () => {
+//   expect(true).toBeTruthy();
+// })
+
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import { MemoryRouter} from 'react-router-dom';
+import 'jest-dom/extend-expect';
+
+import  App from './App';
+import Header from './components/Header';
+
+describe('<Header />', () => {
+    it('should render', () => {
+        const {getByText} = render(<Header />);
+        expect(getByText(/SAUTI DESIGN STUDIO/i)).toBeInTheDocument();
+    });
+  
+})
+
+// can't test with Browser Router Links or NavLinks... which makes it worthless...
