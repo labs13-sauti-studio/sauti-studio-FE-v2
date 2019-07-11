156f9ef1cf5348eb60b9e97ac696273bbbda8444
amberm <nekosenpai@gmail.com>
2019-07-11 13:48:33 -0600

    FE styling near done for landing

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
+import image1 from '../images/image1.svg';
+import image2 from '../images/image2.svg';
 
 import {
   BrowserRouter as Router,
@@ -30,6 +32,42 @@ class Home extends React.Component {
                 <div className="home1">
                     <div className="textbig">
                         <h1>Design Your Own Text-Based Flow</h1>
+                    </div>
+                    
+                </div>
+
+                <div className="home1a">
+                    <div className="homeleft">
+                        <img src={image1}></img>
+                    </div>
+
+                    <div className="homeleft">
+                        <h1>Sharing information made simple & easy with Sauti Design Studio</h1>
+                        <button>STAR A NEW FLOW</button>
+                    </div>
+                    
+                </div>
+
+                <div className="home1a">
+                    
+                    <div className="homesub">
+                        <p>No programming background needed</p>
+                        <br></br>
+                        <p>Create, Update, save & delete flows</p>
+                        <br></br>
+                        <p>Simple & intuitive visual</p>
+                        <br></br>
+                        <p>Cloud-based tool</p>
+                        <br></br>
+                        <p>Click through mockup view</p>
+                        <br></br>
+                        <p>Simple and Easy</p>
+                        <br></br>
+                        <p>No Code Required</p>
+                    </div>
+
+                    <div className="homeleft">
+                        <img src={image2}></img>
                     </div>
                     
                 </div>
