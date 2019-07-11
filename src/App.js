dacfcc5f7b9fc884a2daeaad122b7d09097f01ea
Kevin Tena <kevten09@gmail.com>
2019-07-11 17:52:52 -0500

    Linter files and example

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
-import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {Route} from 'react-router-dom';
+import React from "react"
+import { BrowserRouter as Router } from "react-router-dom"
+import { Route } from "react-router-dom"
 
-import Home from './pages/home.js';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Contact from './components/ContactForm';
-import Footer from './components/Footer';
-import Login from './pages/login';
+import Home from "./pages/home.js"
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+import Contact from "./components/ContactForm"
+import Footer from "./components/Footer"
+import Login from "./pages/login"
 
-import './App.css';
-
+import "./App.css"
 
 function App() {
   return (
     <div>
-
       <div className="App">
-        
         <Header />
-        
+
         <div>
-        
-        <Route exact path="/" component={Home} />
-        
-        <Route path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
 
-        {/* <Router path="/register" component={Register} /> */}
+          <Route path="/login" component={Login} />
 
-      </div>
-        
+          {/* <Router path="/register" component={Register} /> */}
+        </div>
+
         <Footer />
-        
       </div>
-      
-    
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
