f2b7147ebeaa403db6c71ed7821acefdd36bc475
austie702 <austie702@gmail.com>
2019-07-12 02:28:21 -0500

    update profile page with placeholders ready for content from DB

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,17 +1,109 @@
 import React from 'react';
 
-const Profile = () => {
+import Folder from '../components/Folder';
+import Project from '../components/Project';
+
+import blankFolder from '../images/FolderBlank.png';
+import blankProject from '../images/ProjectBlank.png';
+
+const folders = [
+  {
+    id: 1,
+    title: "Church Apps",
+    backgroundImage: blankFolder
+  },
+  {
+    id: 2,
+    title: "Recipe Book",
+    backgroundImage: blankFolder
+  },
+  {
+    id: 3,
+    title: "Personal",
+    backgroundImage: blankFolder
+  },
+  {
+    id: 4,
+    title: "WIP",
+    backgroundImage: blankFolder
+  },
+  {
+    id: 5,
+    title: "Trade Network",
+    backgroundImage: blankFolder
+  }
+]
+
+const projects = [
+  {
+    id: 1,
+    title: "Church Services",
+    backgroundImage: blankProject
+  },
+  {
+    id: 2,
+    title: "Soccer Scheduler",
+    backgroundImage: blankProject
+  },
+  {
+    id: 3,
+    title: "Agriculture Network",
+    backgroundImage: blankProject
+  },
+  {
+    id: 4,
+    title: "And Another",
+    backgroundImage: blankProject
+  },
+  {
+    id: 5,
+    title: "And Another",
+    backgroundImage: blankProject
+  }
+]
+
+const Profile = (props) => {
   return (
     <div className="profile-page-container">
-      <section className="sidebar">
-        <h2>Resources</h2>
+      <section className="resources">
+        <div>
+          <h2>Resources</h2>
+          <div className="resources-list">
+            {projects.map(project => {
+              return <Project
+                key={project.id}
+                title={project.title}
+                bgImage={project.backgroundImage}
+              />
+            })}
+          </div>
+        </div>
       </section>
       <section className="folders-projects">
-        <section className="folders">
-          <h2>Folders</h2>
+        <section className="folders-section">
+            <h2>Folders</h2>
+            <div className="folders-list">
+              {folders.map(folder => {
+                return <Folder
+                key={folder.id}
+                title={folder.title}
+                bgImage={folder.backgroundImage}
+                />
+              })}
+            </div>
         </section>
-        <section className="projects">
-          <h2>Projects</h2>
+
+        <section className="projects-section">
+            <h2>Projects</h2>
+            <div className="projects-list">
+              {projects.map(project => {
+                return <Project
+                  key={project.id}
+                  title={project.title}
+                  bgImage={project.backgroundImage}
+                />
+              })}
+            </div>
         </section>
       </section>
     </div>
