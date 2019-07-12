8b18e115ce91dccb73b10b76c19ff422fef627ec
austie702 <austie702@gmail.com>
2019-07-11 19:29:14 -0500

    add profile page scaffolding to await content fill

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 const Profile = () => {
   return (
-    <h1>Profile Page</h1>
+    <div className="profile-page-container">
+      <section className="sidebar">
+        <h2>Resources</h2>
+      </section>
+      <section className="folders-projects">
+        <section className="folders">
+          <h2>Folders</h2>
+        </section>
+        <section className="projects">
+          <h2>Projects</h2>
+        </section>
+      </section>
+    </div>
   )
 }
 
