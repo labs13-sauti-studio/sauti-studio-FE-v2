cc8dc283117d2586e43ce517d867879cdb91b2b0
austie702 <austie702@gmail.com>
2019-07-12 00:42:00 -0500

    add content to footer component.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import twitter from '../images/twitter48.png';
+import facebook from '../images/facebook48.png';
 
-const Footer = (props) => {
+const Footer = () => {
   return (
-    <div className="AppHead">
-      <h1>Footer Component</h1>
+    <div className="footer">
+      <div className="copyright">
+        <a href="http://sautiafrica.org/company-info/" target="blank">Company Info&nbsp;&nbsp;</a>
+        <p>&nbsp;|&nbsp;&nbsp;Ⓒ 2019&nbsp;&nbsp;</p>
+        <p className="red-text">Sauti Africa Limited.&nbsp;&nbsp;</p>
+        <p>All rights reserved.</p>
+      </div>
+      <div className="footer-social-media">
+        <img src={twitter} alt="twitter logo" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <img src={facebook} alt="twitter logo" />
+      </div>
     </div>
   )
 }
