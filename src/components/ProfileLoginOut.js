4dfc9d6942afc0bc3e2163e4ec3c9d520151012a
austie702 <austie702@gmail.com>
2019-07-17 18:20:22 -0500

    add the app designer page

--- src/components/ProfileLoginOut.js
+++ src/components/ProfileLoginOut.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 const ProfileLoginOut = () => {
   return (
     <div className="navbar-links">
+      <Link to="/workflows">App Builder</Link>
       <Link to="/contact">Contact</Link>
       <Link to="/profile">Profile</Link>
       <Link to="/login">Login Thingy!</Link>
